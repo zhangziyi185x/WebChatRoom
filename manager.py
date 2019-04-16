00e9c94d9afa6a93567afb7ffab858f16354e890
zhang fei <zhang123@163.com>
2019-04-16 16:10:16 +0800

    第二次修改

--- manager.py
+++ manager.py
@@ -17,6 +17,6 @@ app.register_blueprint(get_set)
 app.register_blueprint(chats)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9090, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
 
